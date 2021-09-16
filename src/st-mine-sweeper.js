32a5f274b53592091d6bb3d49ad57bc3609ac6c6
011011100101 <90610032+011011100101@users.noreply.github.com>
2021-09-16 11:56:15 +0500

    Update st-mine-sweeper.js

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -1,29 +1,34 @@
-import { NotImplementedError } from '../extensions/index.js';
+export default function minesweeper(matrix) {
+  const newMatrix = []
+  for (let x of matrix)
+    newMatrix.push(Array(x.length).fill(0))
 
-/**
- * In the popular Minesweeper game you have a board with some mines and those cells
- * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
- * we want to create a Minesweeper game setup.
- *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
- *
- * @example
- * matrix = [
- *  [true, false, false],
- *  [false, true, false],
- *  [false, false, false]
- * ]
- *
- * The result should be following:
- * [
- *  [1, 2, 1],
- *  [2, 1, 1],
- *  [1, 1, 1]
- * ]
- */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix?.[i - 1]?.[j - 1] === true)
+        newMatrix[i][j]++
+
+      if (matrix?.[i - 1]?.[j] === true)
+        newMatrix[i][j]++
+
+      if (matrix?.[i - 1]?.[j + 1] === true)
+        newMatrix[i][j]++
+
+      if (matrix?.[i]?.[j - 1] === true)
+        newMatrix[i][j]++
+
+      if (matrix?.[i]?.[j + 1] === true)
+        newMatrix[i][j]++
+
+      if (matrix?.[i + 1]?.[j - 1] === true)
+        newMatrix[i][j]++
+
+      if (matrix?.[i + 1]?.[j] === true)
+        newMatrix[i][j]++
+
+      if (matrix?.[i + 1]?.[j + 1] === true)
+        newMatrix[i][j]++
+    }
+  }
+  return newMatrix
 }
