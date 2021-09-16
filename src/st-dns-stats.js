9c634f790402304b879de782eea91035e4a9339e
011011100101 <90610032+011011100101@users.noreply.github.com>
2021-09-16 11:59:22 +0500

    Update st-dns-stats.js

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -1,28 +1,14 @@
-import { NotImplementedError } from '../extensions/index.js';
-
-/**
- * Given an array of domains, return the object with the appearances of the DNS.
- *
- * @param {Array} domains
- * @return {Object}
- *
- * @example
- * domains = [
- *  'code.yandex.ru',
- *  'music.yandex.ru',
- *  'yandex.ru'
- * ]
- *
- * The result should be the following:
- * {
- *   '.ru': 3,
- *   '.ru.yandex': 3,
- *   '.ru.yandex.code': 1,
- *   '.ru.yandex.music': 1,
- * }
- *
- */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+  const DNS = {}
+  for (let dom of domains) {
+    let arr = dom.split('.').reverse()
+    for (let i = 0; i < arr.length; i++) {
+      let s = '.' + arr.slice(0,i+1).join('.')
+      if (DNS[s])
+        DNS[s]++
+      else
+        DNS[s]= 1
+    }
+  }
+  return DNS
 }
