a872dc549a29da96d0948ea04256a39032d54e1b
011011100101 <90610032+011011100101@users.noreply.github.com>
2021-09-16 11:48:55 +0500

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,3 +1,9 @@
-export default function countCats(backyard) {
-  return backyard.join(',').split(',').filter(e => e == '^^').length;
+const isString = (str) => (typeof str === 'string');
+
+export default function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  return members.filter(str => isString(str))
+    .map(str => str.trimLeft()[0].toUpperCase())
+    .sort().join('');
 }
