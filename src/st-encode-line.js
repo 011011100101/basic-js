68c07a3e31b2b823a3b45501e7f5f6a7a67ede25
011011100101 <90610032+011011100101@users.noreply.github.com>
2021-09-16 11:58:55 +0500

    Update st-encode-line.js

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,16 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  return str.replace(/(.)\1+/g, x => x.length+x[0])
 }
