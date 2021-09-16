24babb25e47a0492f66baec321d2234875fe32bd
011011100101 <90610032+011011100101@users.noreply.github.com>
2021-09-16 11:51:45 +0500

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,31 +1,54 @@
-import { NotImplementedError } from '../extensions/index.js';
-
-/**
- * Implement class VigenereCipheringMachine that allows us to create
- * direct and reverse ciphering machines according to task description
- * 
- * @example
- * 
- * const directMachine = new VigenereCipheringMachine();
- * 
- * const reverseMachine = new VigenereCipheringMachine(false);
- * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
- * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
- * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
- * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
- */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  reverse = false;
+
+  constructor(isDirect) {
+    if (isDirect == false)
+      this.reverse = true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (message == undefined || key == undefined) throw new Error('Incorrect arguments!');
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let code = ''; let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/)) {
+        let mi = abc.indexOf(message[i]); //алфавитный индекс i-ой буквы сообщения
+        let ki_c = key[(j >= key.length) ? (j % key.length) : j]; //если ключ короткий, повторяем его до длины сообщения
+        let ki = abc.indexOf(ki_c); //алфавитный индекс i-ой буквы ключа
+        let c = abc[(mi + ki) % 26]; // i-ый симфол шифра
+        code += c;
+        j++;
+      } else {
+        code += message[i];
+      }
+    }
+
+    return this.reverse ? code.split('').reverse().join('') : code;
+  }
+
+  decrypt(code, key) {
+    const abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (code == undefined || key == undefined) throw new Error('Incorrect arguments!');
+    code = code.toUpperCase();
+    key = key.toUpperCase();
+    let message = ''; let j = 0;
+
+    for (let i = 0; i < code.length; i++) {
+      if (code[i].match(/[A-Z]/)) {
+        let ci = abc.indexOf(code[i]); //алфавитный индекс i-го символа шифра
+        let ki_c = key[(j >= key.length) ? (j % key.length) : j]; //если ключ короткий, повторяем его до длины сообщения
+        let ki = abc.indexOf(ki_c); //алфавитный индекс i-ой буквы ключа
+        let m = abc[(26 + ci - ki) % 26]; // i-ый симфол сообщения
+        message += m;
+        j++;
+      } else {
+        message += code[i];
+      }
+    }
+
+    return this.reverse ? message.split('').reverse().join('') : message;
   }
 }
